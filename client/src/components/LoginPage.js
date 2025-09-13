9ed8ec147fdf739a166336ed9ddf42c41a08bbbc
Pedro Gomes <pedrogomeslycee@gmail.com>
2025-09-13 18:40:07 +0200

    Refactor code structure and remove redundant changes

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -2,10 +2,23 @@ import "./css/Login.css";
 
 function LoginPage() {
   return (
-    <div className="Login">
-      <h2>Login Page</h2>
-
-      {/* Add your login form here */}
+    <div className="loginContainer">
+      <div className="login">
+        <h2>Login Page</h2>
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <div className="buttonContainer">
+            <button type="submit">Login</button>
+            <button
+              type="button"
+              onClick={() => (window.location.href = "/register")}
+            >
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/RegisterPage.js
@@ -0,0 +1,26 @@
+import "./css/Register.css";
+
+function RegisterPage() {
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterPage;
